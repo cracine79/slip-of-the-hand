76c584b94b91e77661dffccc7659bca417e8df39
CharLee Racine <charleeracine@yahoo.com>
2024-05-04 16:17:12 -0600

    typing all normal keys working - reset approach so hands can be shifted in future

--- src/scripts/keyboard.js
+++ src/scripts/keyboard.js
@@ -12,10 +12,37 @@ const handleKeyDown = function(event){
             if (KEYBOARD[1].slice(0,6).includes(letter)||
                 KEYBOARD[2].slice(0,6).includes(letter)||
                 KEYBOARD[3].slice(0,6).includes(letter)||
-                KEYBOARD[4].slice(0,6).includes(letter)){
+                KEYBOARD[4].slice(0,6).includes(letter)
+            ){
             leftHand.id = `lh_${letter}_pressed`}else{
             rightHand.id = `rh_${letter}_pressed`
             }
+        } else if (event.key === "["){
+            rightHand.id = "rh_lbrac_pressed";
+            keyboard.className = "lbrac_pressed"
+        } else if (event.key === "]"){
+            rightHand.id = "rh_rbrac_pressed";
+            keyboard.className = "rbrac_pressed"
+        } else if (event.key === ";"){
+            rightHand.id = "rh_semicolon_pressed";
+            keyboard.className = "semicolon_pressed";
+        } else if (event.key === ","){
+            rightHand.id = "rh_comma_pressed";
+            keyboard.className = "comma_pressed";
+        } else if (event.key === "."){
+            rightHand.id = "rh_period_pressed";
+            keyboard.className = "period_pressed";
+        } else if (event.key === "/") {
+            rightHand.id = "rh_slash_pressed";
+            keyboard.className = "slash_pressed"
+        } else if (event.key === `'`){
+            rightHand.id = "rh_quote_pressed";
+            keyboard.className = "quote_pressed"
+        } else if (event.code === 'Space'){
+            event.preventDefault();
+            rightHand.id = "rh_space_pressed";
+            leftHand.id="lh_space_pressed";
+            keyboard.className = "space_pressed"
         }
     })
    
