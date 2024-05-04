959532b29681e57774799924da3d7c4fe8a01ffa
CharLee Racine <charleeracine@yahoo.com>
2024-05-03 22:15:47 -0600

    left hand doesn't disappear when right hand keys pressed

--- src/scripts/keyboard.js
+++ src/scripts/keyboard.js
@@ -1,9 +1,21 @@
+const KEYBOARD  = ["1234567890-=".split(""),
+["tab"].concat("qwertyuiop[]".split("")), 
+["caps lock"].concat("asdfghjkl;'".split("")), 
+["shift"].concat("zxcvbnm,./".split("")).concat(["shift"]),
+["","",""," ", " ", " ", " ", " ", " ", "", "", ""]
+]
+
 const handleKeyDown = function(event){
     "abcdefghijklmnopqrstuvwxyz".split("").forEach((letter) => {
         if (event.key === letter){
             keyboard.className = `${letter}_pressed`;
-            leftHand.id = `lh_${letter}_pressed`;
-            rightHand.id = `rh_${letter}_pressed`;
+            if (KEYBOARD[1].slice(0,6).includes(letter)||
+                KEYBOARD[2].slice(0,6).includes(letter)||
+                KEYBOARD[3].slice(0,6).includes(letter)||
+                KEYBOARD[4].slice(0,6).includes(letter)){
+            leftHand.id = `lh_${letter}_pressed`}else{
+            rightHand.id = `rh_${letter}_pressed`
+            }
         }
     })
    
