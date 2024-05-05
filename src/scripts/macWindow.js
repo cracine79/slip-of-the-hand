87a1390eb9f04b22f820f3de466ab6991758174a
CharLee Racine <charleeracine@yahoo.com>
2024-05-04 21:22:07 -0600

    still working on hands

--- src/scripts/macWindow.js
+++ src/scripts/macWindow.js
@@ -12,8 +12,20 @@ class macWindow {
         const keyboard = document.getElementById("keyboard");
         debugger;
         while (i<letters.length){
+            await this.waitForMs(wait); 
+            typedOn.append(letters[i]);
             
-    
+            i++
+        }
+
+    } 
+
+    async keysTypeWords(sentence, typedOn, wait = 30){
+        const letters = sentence.split("");
+        let i = 0;
+        const keyboard = document.getElementById("keyboard");
+     
+        while (i<letters.length){
             await this.waitForMs(wait); 
             typedOn.append(letters[i]);
             if (letters[i] === " "){
@@ -24,8 +36,27 @@ class macWindow {
             i++
         }
         keyboard.className = "unpressed";
-        return
     }
+
+    async handsTypeWords(sentence, typedOn, wait = 30, orig){
+        const origLetters = sentence.split("")
+        const letters = sentence.split("");
+        let i = 0;
+        const keyboard = document.getElementById("keyboard");
+    
+        while (i<letters.length){
+            await this.waitForMs(wait); 
+            typedOn.append(letters[i]);
+            if (letters[i] === " "){
+                keyboard.className = "space_pressed"
+            } else {
+                keyboard.className = `${letters[i].toLowerCase()}_pressed`
+            }
+            i++
+        }
+        keyboard.className = "unpressed";
+    }
+
  
     waitForMs(ms){
         return new Promise(resolve => setTimeout(resolve, ms)) 
@@ -43,13 +74,13 @@ class macWindow {
         await this.typeWords(intro2, para2);
         const dry = "DRY  ";
         const drySpot = document.getElementById("dry");
-        await this.typeWords(dry, drySpot,500);
+        await this.keysTypeWords(dry, drySpot,700);
         const intro2Cont= " but accidentally started out typing one key position to the left.  This would result in typing the word: "
         const para2Cont = document.getElementById("introText2Cont")
         await this.typeWords(intro2Cont, para2Cont);
         const set = "SET  ";
         const setSpot = document.getElementById("set");
-        await this.typeWords(set, setSpot,500);
+        await this.keysTypeWords(set, setSpot,500);
         const intro3 = "Go ahead, give it a try!  Enter a word of phrase and see what slips of the hand you could have had!"
         const para3 = document.getElementById("introText3");
         await this.typeWords(intro3, para3)
