79e6f8f7d41f6b70dd6c2e38e1f1f1fb57987a29
CharLee Racine <charleeracine@yahoo.com>
2024-05-05 09:43:49 -0600

    backtracking - need to find new way to register individual hand shifts - look to existing methods in Word class

--- src/scripts/macWindow.js
+++ src/scripts/macWindow.js
@@ -1,4 +1,8 @@
+import Word from "./word.js"
+
+
 class macWindow {
+
 
     constructor(){
         this.view = document.getElementById("inner-monitor");
@@ -10,7 +14,7 @@ class macWindow {
         const letters = sentence.split("");
         let i = 0;
         const keyboard = document.getElementById("keyboard");
-        debugger;
+     
         while (i<letters.length){
             await this.waitForMs(wait); 
             typedOn.append(letters[i]);
@@ -41,22 +45,51 @@ class macWindow {
     }
 
     async handsTypeWords(sentence, typedOn, wait = 30, orig){
-        const origLetters = sentence.split("")
-        const letters = sentence.split("");
+        const origLetters = new Word(orig);
+        const letters = new Word(sentence);
+        const origLettersArr = orig.split("");
+        const lettersArr = sentence.split("");
+        
+    
+        // const leftHandShift = [leftVertShift, leftHorizShift]
+        // const rightHandShift = [rightVertShift, rightHorizShift]
+
+      
+
         let i = 0;
         const keyboard = document.getElementById("keyboard");
-    
-        while (i<letters.length){
-            await this.waitForMs(wait); 
-            typedOn.append(letters[i]);
-            if (letters[i] === " "){
+        const leftHand = document.getElementById("lh_unpressed");
+        const rightHand = document.getElementById("rh_unpressed");
+        
+        while (i<lettersArr.length){
+            const left = "qwertgfdsazxcvb".split("");
+            const right = "yuiophjklnm".split("")
+ 
+        
+            typedOn.append(lettersArr[i]);
+            if (lettersArr[i] === " "){
                 keyboard.className = "space_pressed"
+                rightHandShift.id = "rh_space_pressed"
+
             } else {
-                keyboard.className = `${letters[i].toLowerCase()}_pressed`
+                keyboard.className = `${lettersArr[i].toLowerCase()}_pressed`
             }
+
+            
+            if (left.includes(lettersArr[i].toLowerCase())){
+                debugger;
+                leftHand.id = `lh_${lettersArr[i].toLowerCase()}_pressed`
+                rightHand.id = "rh_unpressed"
+            } else if (right.includes(lettersArr[i].toLowerCase())){
+                rightHand.id = `rh_${lettersArr[i].toLowerCase()}_pressed`
+                leftHand.id = "lh_unpressed"
+            }
+            await this.waitForMs(wait); 
             i++
         }
         keyboard.className = "unpressed";
+        leftHand.id = `lh_unpressed`;
+        rightHand.id = `rh_unpressed`;
     }
 
  
@@ -76,13 +109,13 @@ class macWindow {
         await this.typeWords(intro2, para2);
         const dry = "DRY";
         const drySpot = document.getElementById("dry");
-        await this.keysTypeWords(dry, drySpot,700);
+        await this.handsTypeWords(dry, drySpot,700, "dry");
         const intro2Cont= " but accidentally started out typing one key position to the left.  This would result in typing the word: "
         const para2Cont = document.getElementById("introText2Cont")
         await this.typeWords(intro2Cont, para2Cont);
         const set = "SET";
         const setSpot = document.getElementById("set");
-        await this.keysTypeWords(set, setSpot,500);
+        await this.handsTypeWords(set, setSpot,500, "dry");
         await this.waitForMs(1000); 
         const slip= "SLIP OF THE HAND!!"
         const slipSpot = document.getElementById("slip")
